fb5ba33efba5c421e1d1fb9c65a6939589252731
tortwz <heitormacedo1234@gmail.com>
2026-02-04 16:15:31 -0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+// Variables and Mutability
+fn main() {
+    println!("Hello, world!");
+    let mut a = 5;
+    println!("The value of a is {}", a);
+    a = 10;
+    println!("The value of a is now {}", a);
+}
